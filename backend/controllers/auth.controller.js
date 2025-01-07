ed1440979bb5ae3e5cc28e2171c722bcd4c063ef
Maciek-Sawicki <macieksawickim@gmail.com>
2025-01-07 19:56:09 +0100

    create user schema and signup endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,62 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../libs/utils/generateToken.js";
+
 export const signup = async (req, res) => {
-    res.status(200).json({ message: 'Signup route' });
+    try {
+        const { username, fullName, email, password } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email" });
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            fullName,
+            email,
+            password: passwordHash
+        })
+
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            
+            res.status(201).json({
+                _id: newUser._id,
+                username: newUser.username,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profilePicture: newUser.profilePicture,
+                coverImage: newUser.coverImage,
+                bio: newUser.bio
+            })
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ message: "Server error" });
     }
+
+   }
---
+++ backend/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverImage: {
+        type: String,
+        default: ""
+    },
+    bio: {
+        type: String,
+        default: ""
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

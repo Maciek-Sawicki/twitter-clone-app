f56c5a34fdbd9500e20db27cc7d7977b93005c33
Maciek-Sawicki <macieksawickim@gmail.com>
2025-01-31 01:58:16 +0100

    add saving registration inputs to local storage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,38 +1,3 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchPosts } from "../store/slices/postSlice";
-// import { RootState, AppDispatch } from "../store/store";
-
-// const Home = () => {
-//     const dispatch = useDispatch<AppDispatch>();
-//     const { posts, loading, error } = useSelector((state: RootState) => state.posts);
-
-//     useEffect(() => {
-//         dispatch(fetchPosts());
-//     }, [dispatch]);
-
-//     if (loading) return <p>Loading posts...</p>;
-//     if (error) return <p style={{ color: "red" }}>{error}</p>;
-
-//     const safePosts = Array.isArray(posts) ? posts : [];
-
-//     return (
-//         <div>
-//           <h1>Strona główna</h1>
-//           {safePosts.length === 0 ? <p>Brak postów</p> : safePosts.map((post) => (
-//             <div key={post._id} className="card">
-//               <h3>@{post.postedBy.username}</h3>
-//               <p>{post.text}</p>
-//               <p>❤️ {post.likes.length}</p>
-//               <p>💬 {post.comments.length} komentarzy</p>
-//             </div>
-//           ))}
-//         </div>
-//       );
-//     };
-
-// export default Home;
-
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { useNavigate } from "react-router-dom";
@@ -65,3 +30,18 @@ const Home = () => {
 };
 
 export default Home;
+
+//     return (
+//         <div>
+//           <h1>Strona główna</h1>
+//           {safePosts.length === 0 ? <p>Brak postów</p> : safePosts.map((post) => (
+//             <div key={post._id} className="card">
+//               <h3>@{post.postedBy.username}</h3>
+//               <p>{post.text}</p>
+//               <p>❤️ {post.likes.length}</p>
+//               <p>💬 {post.comments.length} komentarzy</p>
+//             </div>
+//           ))}
+//         </div>
+//       );
+//     };
